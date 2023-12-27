cd849d50b226b7c8aa0da5a6b7d052892f50bb8a
Steve Livingston <slivingston@solarvalleysolutions.com>
2023-12-27 13:12:33 -0500

    test

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from vii import InanimateIntransitive_Stemmer
 
 app = Flask(__name__)
 
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    return 'Test'
+@app.route('/stem/vii/<string:verb>', methods=['GET'])
+def stem(verb):
+    vii = InanimateIntransitive_Stemmer()
+    return vii.is_potential_vii_ending(verb)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
